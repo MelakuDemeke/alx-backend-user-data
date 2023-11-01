d9a5015081b203f96e9ba74570d3b88937b58e87
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-01 12:26:27 +0300

    type anotiate the arguments

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+"""module to filter logs"""
+from typing import List
 
-def filter_datum(fields, rdaction, message, separator):
+def filter_datum(
+    fields: List[str],
+    rdaction: str,
+    message: str,
+    separator: str):
     pass
