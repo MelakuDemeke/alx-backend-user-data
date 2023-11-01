0b752ce0d561ac3cbca4475b10fa9a42854eca2b
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-01 13:18:19 +0300

    define patterns

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,12 @@
 """module to filter logs"""
 from typing import List
 
+
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+
 def filter_datum(
     fields: List[str], rdaction: str,
     message: str, separator: str):
