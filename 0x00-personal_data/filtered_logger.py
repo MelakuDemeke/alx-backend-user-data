f54d0fddef190520830aeb9a908d34a23aee20c1
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-02 11:21:17 +0300

    remvoe doc

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,20 +10,10 @@ patterns = {
 }
 
 
-def filter_datum(fields: List[str], rdaction: str, message: str, separator: str
-                 ) -> str:
-    """
-    Replaces all occurrences of certain fields in a message with a given
-    redaction string.
-
-    Args:
-        fields (List[str]): A list of fields to be filtered.
-        rdaction (str): The string to replace filtered fields with.
-        message (str): The message to be filtered.
-        separator (str): The separator used to split the message into fields.
-
-    Returns:
-        str: The filtered message.
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str,
+        ) -> str:
+    """Filters a log line.
     """
     extract, replace = (patterns["extract"], patterns["replace"])
     return re.sub(extract(fields, separator), replace(redaction), message)
