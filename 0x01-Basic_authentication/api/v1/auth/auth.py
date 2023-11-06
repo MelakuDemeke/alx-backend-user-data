2e7e8e4423cf107e801c4ed6d145ad4f7001d6d6
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-06 13:01:49 +0300

    decrease the doc

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,14 +11,6 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         Checks if a request to a given path requires authentication or not.
-
-        Args:
-            path (str): The path of the request.
-            excluded_paths (List[str]): A list of paths that do not require
-                                        authentication.
-
-        Returns:
-            bool: True if authentication is required, False otherwise.
         """
         if path is not None and excluded_paths is not None:
             for exclusion_path in map(lambda x: x.strip(), excluded_paths):
