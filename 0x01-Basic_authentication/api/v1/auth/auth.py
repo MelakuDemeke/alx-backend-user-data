a7043e57163e5d7729027f9c1bf6168ba84be487
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-06 12:52:21 +0300

    define authorization_header func

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,3 +9,6 @@ class Auth:
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         pass
+
+    def authorization_header(self, request=None) -> str:
+        pass
