5fa885efad5fcf46596659644f777aafbf515bd4
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-06 16:44:03 +0300

    def user_object_from_credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -53,3 +53,7 @@ class BasicAuth(Auth):
                 password = field_match.group('password')
                 return user, password
         return None, None
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        pass
