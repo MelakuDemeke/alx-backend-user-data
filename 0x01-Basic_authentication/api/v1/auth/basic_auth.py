999cd2f8e179c6c5b72a40c4900ee1eb1853b1db
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-06 16:44:53 +0300

    add doc

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -56,4 +56,6 @@ class BasicAuth(Auth):
 
     def user_object_from_credentials(
             self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """get a user based on user auth credential
+        """
         pass
