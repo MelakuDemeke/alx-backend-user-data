8bde8ed2ee9cd72755994faa6000153f26c64f19
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-06 12:55:46 +0300

    add doc to require auth

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -4,10 +4,22 @@
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth:
     """ Auth class
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Checks if a request to a given path requires authentication or not.
+
+        Args:
+            path (str): The path of the request.
+            excluded_paths (List[str]): A list of paths that do not require
+                                        authentication.
+
+        Returns:
+            bool: True if authentication is required, False otherwise.
+        """
         pass
 
     def authorization_header(self, request=None) -> str:
