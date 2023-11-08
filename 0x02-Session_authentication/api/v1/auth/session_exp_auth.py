d963507d8d25c50cd1039253261a28358656e100
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-08 18:52:51 +0300

    get user id from session id

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -28,3 +28,19 @@ class SessionExpAuth(SessionAuth):
             'created_at': datetime.now(),
         }
         return session_id
+
+    def user_id_for_session_id(self, session_id=None) -> str:
+        """returns a User ID based on a Session ID
+        """
+        if session_id in self.user_id_by_session_id:
+            session_dict = self.user_id_by_session_id[session_id]
+            if self.session_duration <= 0:
+                return session_dict['user_id']
+            if 'created_at' not in session_dict:
+                return None
+            cur_time = datetime.now()
+            time_span = timedelta(seconds=self.session_duration)
+            exp_time = session_dict['created_at'] + time_span
+            if exp_time < cur_time:
+                return None
+            return session_dict['user_id']
