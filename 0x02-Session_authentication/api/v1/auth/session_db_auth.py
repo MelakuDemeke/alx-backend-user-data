d23ed08499c0cb6f426f34c89da2bf593e0918b7
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-08 22:13:19 +0300

    def session exp auth

---
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""session db auth module
+"""
+from .session_exp_auth import SessionExpAuth
+
+
+class SessionDBAuth(SessionExpAuth):
+    """session suth with db
+    """
+    pass
