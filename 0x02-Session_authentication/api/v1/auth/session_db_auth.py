2aeb7e52603a4b4a0fc51c74eddaed2616525f68
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-08 22:14:17 +0300

    create session for user

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -7,4 +7,15 @@ from .session_exp_auth import SessionExpAuth
 class SessionDBAuth(SessionExpAuth):
     """session suth with db
     """
-    pass
+    def create_session(self, user_id=None) -> str:
+        """create and store session id for usercrea
+        """
+        session_id = super().create_session(user_id)
+        if type(session_id) == str:
+            kwargs = {
+                'user_id': user_id,
+                'session_id': session_id,
+            }
+            user_session = UserSession(**kwargs)
+            user_session.save()
+            return session_id
