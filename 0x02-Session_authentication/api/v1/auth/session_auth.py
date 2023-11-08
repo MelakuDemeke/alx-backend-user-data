aa68cc0907d42f1b7fd635a9e207f82b880fd2cc
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-08 17:09:57 +0300

    def curent user

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -25,3 +25,5 @@ class SessionAuth(Auth):
         if type(session_id) is str:
             return self.user_id_by_session_id.get(session_id)
 
+    def current_user(self, request=None) -> User:
+        pass
