aeded9781c3ba4f355c5570d06f02eeb77471257
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-08 14:01:34 +0300

    import flask and auth

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """Session Authentication module for v1 API.
 """
+from flask import request
+
+from .auth import Auth
 
 class SessionAuth(Auth):
     """define Session auth class
