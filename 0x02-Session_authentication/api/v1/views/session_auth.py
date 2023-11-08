b442cf46b3fd6c7f9267c8f62fc5cd3939baff00
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-08 17:49:51 +0300

    define new route

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 """This module contains views for session authentication.
 """
+from typing import Tuple
 
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login() -> Tuple[str, int]:
+    pass
