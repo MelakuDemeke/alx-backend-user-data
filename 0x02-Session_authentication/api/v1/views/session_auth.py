78b815bd91b41fd56fead213d778799e07c8dd69
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-08 17:50:44 +0300

    add doc

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -6,4 +6,9 @@ from typing import Tuple
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def login() -> Tuple[str, int]:
+    """Log in a user using session authentication.
+    Returns:
+        Tuple containing a JSON representation of a User object and an
+        HTTP status code.
+    """
     pass
