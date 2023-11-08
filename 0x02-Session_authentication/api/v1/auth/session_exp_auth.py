de94b482a7af1614433044d97c3826d4ef32a6e5
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-08 18:51:36 +0300

    create sessinn id

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -16,3 +16,15 @@ class SessionExpAuth(SessionAuth):
             self.session_duration = int(os.getenv('SESSION_DURATION', '0'))
         except Exception:
             self.session_duration = 0
+
+    def create_session(self, user_id=None):
+        """new session id for new user
+        """
+        session_id = super().create_session(user_id)
+        if type(session_id) != str:
+            return None
+        self.user_id_by_session_id[session_id] = {
+            'user_id': user_id,
+            'created_at': datetime.now(),
+        }
+        return session_id
