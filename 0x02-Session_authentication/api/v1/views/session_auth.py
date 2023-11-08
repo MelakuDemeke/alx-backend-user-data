210ed9b67f921969dd0d4ec9143a7bfeaa4ba5c3
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-08 18:16:59 +0300

    move destory code to auth

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -35,13 +35,3 @@ def login() -> Tuple[str, int]:
         return res
     return jsonify({"error": "wrong password"}), 401
 
-def destroy_session(self, request=None):
-        """Destroys an auth session.
-        """
-        session_id = self.session_cookie(request)
-        user_id = self.user_id_for_session_id(session_id)
-        if (request is None or session_id is None) or user_id is None:
-            return False
-        if session_id in self.user_id_by_session_id:
-            del self.user_id_by_session_id[session_id]
-        return True
