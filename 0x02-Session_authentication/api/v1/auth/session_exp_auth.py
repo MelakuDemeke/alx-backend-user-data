ce53cd6b96554df24ada267baaf2e56aef717503
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-08 18:50:50 +0300

    add init method with  session duration

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 """provides functionality for session authentication with expiration
 """
+import os
 from .session_auth import SessionAuth
 
 
 class SessionExpAuth(SessionAuth):
-    """Session authentication class with expiration.
+    """Inherits from SessionAuth and addssession expiration functionality.
     """
-    pass
+    def __init__(self) -> None:
+        """init new session Exp auth
+        """
+        super().__init__()
+        try:
+            self.session_duration = int(os.getenv('SESSION_DURATION', '0'))
+        except Exception:
+            self.session_duration = 0
