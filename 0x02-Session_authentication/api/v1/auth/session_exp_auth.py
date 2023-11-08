9fcf8a7dd210f83d0f859e357e2cecb0b02dfd41
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-08 18:49:06 +0300

    def class session Exp auth that extend sessionauth

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 """provides functionality for session authentication with expiration
 """
+from .session_auth import SessionAuth
 
+
+class SessionExpAuth(SessionAuth):
+    """Session authentication class with expiration.
+    """
+    pass
