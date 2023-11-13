6ecf3d5daba79c6fecc84f53bc21942decf6ffec
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-13 16:56:04 +0300

    Add check for existing user in Auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -25,3 +25,4 @@ class Auth:
             self._db.find_user_by(email=email)
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+        raise ValueError("User {} already exists".format(email))
