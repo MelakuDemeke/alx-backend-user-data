da51bd57ba100370cfbf6e86a797571b03e14203
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-13 16:51:34 +0300

    add given code

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,9 +2,18 @@
 """Auth module
 """
 import bcrypt
+from db import DB
 
 
 def _hash_password(password: str) -> bytes:
     """Hashes a password using bcrypt.
     """
     return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
