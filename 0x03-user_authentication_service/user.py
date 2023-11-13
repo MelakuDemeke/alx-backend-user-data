969153da7c2be019059d868a872efcd37018b2e7
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-13 11:39:18 +0300

    Add declarative base to User model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 """Defines the User class Model for the users table in the database
 """
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
 
 
-class User():
+class User(Base):
     pass
