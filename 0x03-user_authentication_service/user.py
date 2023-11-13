2e744c9456ee528b33d54ac3373292e533a1da57
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-13 11:42:55 +0300

    Add new columns to User table

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -10,3 +10,8 @@ class User(Base):
     """Representation of a user instance in the database
     """
     __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    hashed_password = Column(String(250), nullable=False)
+    session_id = Column(String(250), nullable=True)
+    reset_token = Column(String(250), nullable=True)
