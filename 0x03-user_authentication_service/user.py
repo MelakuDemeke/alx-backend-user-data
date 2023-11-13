9337af353e3fa6eaf724a12c54239fb89b1a8dab
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-13 11:42:27 +0300

    Add import statement for sqlalchemy in User class Model

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Defines the User class Model for the users table in the database
 """
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
