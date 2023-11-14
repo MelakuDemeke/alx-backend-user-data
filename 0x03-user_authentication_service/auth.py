aae50c8a29c8565fa5e4d93e6d5e043bcf153ed3
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-14 10:38:58 +0300

    Add session ID deletion functionality to destroy_session method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -81,4 +81,6 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         """Destroys a user session.
         """
-        pass
+        if user_id is None:
+            return None
+        self._db.update_user(user_id, session_id=None)
