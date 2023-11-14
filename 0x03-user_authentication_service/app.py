a31c447400e97f2097d8691155cf0552d7c7e369
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-14 10:47:37 +0300

    Add profile route to app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -60,5 +60,10 @@ def logout() -> str:
     return redirect("/")
 
 
+@app.route("/profile", methods=["GET"], strict_slashes=False)
+def profile() -> str:
+    pass
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
