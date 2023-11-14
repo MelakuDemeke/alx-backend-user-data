4e469113ed5ea555ae95270c6366c733cfeb2876
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-14 10:48:44 +0300

    Add user profile endpoint authentication

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -66,7 +66,11 @@ def profile() -> str:
     Return:
         - profile payload
     """
-    pass
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    return jsonify({"email": user.email})
 
 
 if __name__ == "__main__":
