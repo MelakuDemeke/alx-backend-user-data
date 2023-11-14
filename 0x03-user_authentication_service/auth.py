595bec627ec27eb5bc10806b45586dbac6ad31dd
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-14 11:46:53 +0300

    Update user password and reset token in Auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -102,4 +102,16 @@ class Auth:
     def update_password(self, reset_token: str, password: str) -> None:
         """Updates a user password.
         """
-        pass
+        user = None
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+        except NoResultFound:
+            user = None
+        if user is None:
+            raise ValueError()
+        updated_hash_pwd = _hash_password(password)
+        self._db.update_user(
+            user.id,
+            hashed_password=updated_hash_pwd,
+            reset_token=None,
+        )
